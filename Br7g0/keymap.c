54f29d101825a718044eed1642f00bf053c018fe
leonardoacosta <leo@leonardoacosta.dev>
2026-07-31 13:38:35 -0500

    Restore grave tap and Escape hold

--- Br7g0/keymap.c
+++ Br7g0/keymap.c
@@ -28,10 +28,11 @@ enum tap_dance_codes {
 };
 
 #define DUAL_FUNC_0 LT(4, KC_F8)
+#define DUAL_FUNC_1 LT(5, KC_F7)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    KC_GRAVE,           KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
+    DUAL_FUNC_1,           KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     DUAL_FUNC_0,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     MT(MOD_LSFT, KC_BSPC),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RSFT, KC_QUOTE),
     KC_LEFT_GUI,    MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_RIGHT_CTRL,  
@@ -157,6 +158,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           unregister_code16(KC_ESCAPE);
         }  
       }  
+      return false;
+    case DUAL_FUNC_1:
+      if (record->tap.count > 0) {
+        if (record->event.pressed) {
+          register_code16(KC_GRAVE);
+        } else {
+          unregister_code16(KC_GRAVE);
+        }
+      } else {
+        register_code16(KC_ESCAPE);
+        unregister_code16(KC_ESCAPE);
+      }
       return false;
     case CUSTOM_GLOBE:
       // Momentary consumer-page report, same pattern QMK uses for other
